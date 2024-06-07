d3231b5b55cfd5c6b9c87c5c7206e5b1f936fc9d
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-06-07 15:58:56 +0500

    4-oy 6-dars

---
+++ index.js
@@ -0,0 +1,40 @@
+
+// 1 - masala
+// let n = 50;
+// for (let i = 1; i <= n; i++) {
+//     if (i % 2 == 1) {
+//         console.log(i * i);
+//     }
+// }
+// 2 - masala
+// let n = 10
+// for (let i = 1; i <= n; i++) {
+//     console.log(i * i * i);
+// }
+// 3 - masala
+// let n = 20
+// let faktor=1n
+// for (let i = 1n; i <= n; i++) {
+// faktor=faktor*i;
+// console.log(faktor);
+// }
+// 4 - masala
+// let n =30
+// let result = 0;
+// for (let i = 1; i <= n; i++) {
+//     result += i * i;
+// }
+// console.log(result);
+// 5-masala
+//let counter;
+//for(i=1;i<=20;i++){
+//counter+
+//}
+// 6-masala
+// let n = 100
+// for (let i = 1; i <= n; i++){
+// if (i * 2 < n) {
+//     console.log(i);
+// }
+// }
+
